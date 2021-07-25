805097ffa910d5825c25b642db5e1dedcad921b0
Muddasir E <muddasir.e@bigbinary.com>
2021-07-26 00:19:21 +0530

    Added tasks controller and index action

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_07_25_173922) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "body"
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
