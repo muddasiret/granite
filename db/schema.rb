893ddfccb7d31b59f724ccfd1a9f8f74bc9f0a7b
Muddasir E <muddasir.e@bigbinary.com>
2021-07-27 21:54:33 +0530

    Added slug to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_062656) do
+ActiveRecord::Schema.define(version: 2021_07_27_155036) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug", null: false
+    t.index ["slug"], name: "index_tasks_on_slug", unique: true
   end
 
 end
