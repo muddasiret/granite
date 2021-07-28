fd20dc7cceaba9e2e36b50cd351c8f0807208115
Muddasir E <muddasir.e@bigbinary.com>
2021-07-29 00:34:46 +0530

    Added signup feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     users = User.all.as_json(only: %i[id name])
     render status: :ok, json: { users: users }
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render status: :ok, json: { notice: 'User was successfully created!' }
+    else
+      render status: :unprocessable_entity, json: {}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  
 end
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -7,6 +7,7 @@ import ShowTask from "components/Tasks/ShowTask";
 import Dashboard from "components/Dashboard";
 import { ToastContainer } from "react-toastify";
 import EditTask from "components/Tasks/EditTask";
+import Signup from "components/Authentication/Signup";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -21,6 +22,7 @@ const App = () => {
     <Router>
       <ToastContainer />
       <Switch>
+        <Route exact path="/signup" component={Signup} />
         <Route exact path="/tasks/:slug/edit" component={EditTask} />
         <Route exact path="/tasks/:slug/show" component={ShowTask} />
         <Route exact path="/tasks/create" component={CreateTask} />
