da71e7f0fb03cd6010cd095c3a6d9dd6dabf0049
Muddasir E <muddasir.e@bigbinary.com>
2021-07-29 00:21:50 +0530

    Added has_secure_password and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: { maximum: 35 }
+  has_secure_password
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, on: :create
+
+  private
+
+    def to_lowercase
+      email.downcase!
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_183236) do
+ActiveRecord::Schema.define(version: 2021_07_28_184256) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2021_07_28_183236) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
